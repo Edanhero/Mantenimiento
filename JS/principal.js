0a25f951f7564519c302a6aca6e51317bff5aeac
Edanhero <eahernandezo@ufpso.edu.co>
2024-10-22 13:56:31 -0500

    cambios de orden

---
+++ JS/principal.js
@@ -0,0 +1,26 @@
+console.log ( "hola mundo");
+
+function miFuncion(){
+    var variableLocal="soy una variable local";
+    console.log(variableLocal);
+}
+
+miFuncion();
+
+function funcionSecun(){
+    let nombre="edinson"
+    let edad= "19"
+    let esMayorEdad= true;
+    console.log(nombre,edad,esMayorEdad);
+}
+
+funcionSecun();
+
+let numeros=[1,2,3,4,5];
+console.log(numeros);
+
+console.log("hola");
+
+let resultado=2 ** 3;
+console.log(resultado);
+
